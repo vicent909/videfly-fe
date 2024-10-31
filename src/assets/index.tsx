b18280c9495f65ffc138e50cc45a006fd8efbf23
Vincent <vincentcan909@gmail.com>
2024-10-31 19:04:32 +0700

    Feat: sidebar

---
+++ src/assets/index.tsx
@@ -0,0 +1,37 @@
+import Logo from "./Logo.png";
+import AddLogo from "./add-circle-half-dot.svg";
+import Home from "./home-03.svg";
+import CalendarIcon from "./calendar-03.svg";
+import CalendarIconActive from "./calendar-03-active.svg";
+import ProyekIcon from "./folder-library.svg";
+import ProyekIconActive from "./folder-library-active.svg";
+import AIIcon from "./streamline_ai-edit-spark.svg";
+import AIIconActive from "./streamline_ai-edit-spark-active.svg";
+import AvatarsIcon from "./user-star-02.svg";
+import AvatarsIconActive from "./user-star-02-active.svg";
+import BrandKitIcon from "./heroicons-outline_color-swatch.svg";
+import BrandKitIconActive from "./heroicons-outline_color-swatch-active.svg";
+import HomeActive from "./home-03-active.svg";
+import Setting from "./settings-02.svg";
+import SettingActive from "./settings-02-active.svg";
+import LogoutIcon from "./logout-03.svg";
+
+export {
+  Logo,
+  AddLogo,
+  Home,
+  CalendarIcon,
+  ProyekIcon,
+  AIIcon,
+  AvatarsIcon,
+  BrandKitIcon,
+  HomeActive,
+  Setting,
+  LogoutIcon,
+  CalendarIconActive,
+  ProyekIconActive,
+  AIIconActive,
+  AvatarsIconActive,
+  BrandKitIconActive,
+  SettingActive
+};
